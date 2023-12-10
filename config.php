001a506e2b88bc3cb163497f50320ad09e492e5c
Lê Văn Lực - IT <143623207+Levanluc22@users.noreply.github.com>
2023-12-10 16:08:24 +0700

    chỉnh ngày 10/12

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('localhost','root','','tltgift_db') or die('connection failed');
 
 ?>
--- footer.php
+++ footer.php
@@ -29,8 +29,9 @@
       <div class="box">
          <h3>Theo dõi chúng tôi</h3>
          <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
-         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-twitter "></i> Twitter </a>
          <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+       
         
       </div>
 
